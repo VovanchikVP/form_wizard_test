40bd799226c2d37a4f5cdddd4d3881af1393239e
Vladimir <kapitonov.vladimir@list.ru>
2025-07-12 22:10:17 +1000

    Добавлена переменная

--- src/configs/config.py
+++ src/configs/config.py
@@ -8,6 +8,7 @@ load_dotenv()
 
 class Settings(BaseSettings):
     BOT_TOKEN: str = os.environ.get("APP_BOT_TOKEN", "")
+    BOT_TOKEN2: str = os.environ.get("APP_BOT_TOKEN", "2")
     TIME_OUT: int = os.environ.get("APP_TIMEOUT", 100)
 
 
