07bd16e1b0bd5c15373d96487aa200a8af564ba5
Vladimir <kapitonov.vladimir@list.ru>
2025-07-12 22:13:33 +1000

    fix

--- src/configs/config.py
+++ src/configs/config.py
@@ -8,7 +8,6 @@ load_dotenv()
 
 class Settings(BaseSettings):
     BOT_TOKEN: str = os.environ.get("APP_BOT_TOKEN", "")
-    BOT_TOKEN2: str = os.environ.get("APP_BOT_TOKEN", "2")
     TIME_OUT: int = os.environ.get("APP_TIMEOUT", 100)
 
 
